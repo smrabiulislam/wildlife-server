a73d1e3f14754ee20fdb6eb756f794c14bcc56ba
smrabiulislam <smarbiulislam03@gmail.com>
2022-11-08 13:57:04 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+
 const app = express();
 const port = process.env.port || 5000;
 
@@ -9,10 +10,8 @@ const port = process.env.port || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-console.log(process.env.DB_USER)
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.dkuqjjn.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dkuqjjn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
