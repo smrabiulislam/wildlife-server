60605dab9e8f6a7c5c77d8b1eda1167987ba9fbd
smrabiulislam <smarbiulislam03@gmail.com>
2022-11-08 14:22:00 +0600

    database updated

--- index.js
+++ index.js
@@ -11,13 +11,36 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dkuqjjn.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect()
+        console.log('connect')
+        const serviceCollection = client.db('wildLife').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
