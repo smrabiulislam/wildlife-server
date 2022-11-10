4c1f1a6fec6bd22733ce06b2329e50a2b215333d
smrabiulislam <smarbiulislam03@gmail.com>
2022-11-10 23:22:46 +0600

    server

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dkuqjjn.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -31,16 +30,18 @@ async function run() {
 
         app.get("/services-home", async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query).limit(3);
+            const cursor = serviceCollection.find(query).limit(3).sort({ _id: -01 });
             const services = await cursor.toArray();
             res.send(services);
         });
 
         app.get('/services', async (req, res) => {
+            console.log(uri)
             const query = {}
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).sort({ _id: -01 });
             const services = await cursor.toArray();
             res.send(services);
+
         });
 
         app.get('/services/:id', async (req, res) => {
